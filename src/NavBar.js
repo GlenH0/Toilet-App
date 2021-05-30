d0b60c110bcba59d37076ef164725f5584fa3e8e
bobross96 <zadus96@gmail.com>
2021-05-31 00:41:56 +0800

    removed navbar css classes from index, changed inline style on navbar to test

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom'
+import './NavBar.css';
 
 const NavBar = () => {
     return ( 
         <div className="navBar">
             <div className="links">
-                <Link to="/" className="links-btn" style={{color: 'black'}}>Home</Link>
-                <Link to="/map" className="links-btn"  style={{color: 'black'}}>Map</Link>
+                <Link to="/" className="links-btn" >Home</Link>
+                <Link to="/map" className="links-btn">Map</Link>
             </div>
         </div>
      );
