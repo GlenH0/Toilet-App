5427344a9e8a57177e005399515eb77d7993c7be
Glen Ho <glenhck@hotmail.com>
2021-06-01 03:44:23 +0800

    helllo

---
+++ src/useFetch(s)/fetchReviews.js
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react'
+
+const useReviewFetch = (url) => {
+    const [review, setReview] = useState([]);
+    const [reviewErr, setReviewError] = useState(null); 
+
+
+    useEffect(() => {
+        const abortConst = new AbortController();
+
+        fetch(url, {signal: abortConst.signal})
+        .then(res => {
+            console.log(res)
+            if(!res.ok){
+                throw Error("NOOB")
+            }
+            return res.json();
+        })
+        .then((review) => {
+            setReview(review);
+            setReviewError(null);
+        })
+        .catch((err) => {
+            if(err.name === 'AbortError'){
+                console.log('no fetch')
+            }
+            else{
+                setReviewError(err.message)
+            }
+        })
+
+        return () => abortConst.abort()
+    }, [url])
+    
+    return {review, reviewErr, setReview}
+}
+ 
+export default useReviewFetch;
