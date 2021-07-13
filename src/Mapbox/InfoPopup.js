8982f03bfde40319c86d68da4d287c03515d7954
Glen Ho <glenhck@hotmail.com>
2021-07-13 22:24:36 +0800

    Fixed Link issue in infoPopup

--- src/Mapbox/InfoPopup.js
+++ src/Mapbox/InfoPopup.js
@@ -1,18 +1,26 @@
 import './InfoPopup.css'
 import ReactStars from "react-rating-stars-component";
+import { BrowserRouter, Link } from 'react-router-dom';
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 const InfoPopup = (props) => {
-    
-
     return (
         <div className="InfoPopup">
             <h2>{props.name}</h2>
-            <a href={"/toiletdetails/" + props._id}>
+            {/* previous solution */}
+            {/* <a href={"/toiletdetails/" + props._id}>
             <div className='InfoPopup-imageContainer'>
              <img className="InfoPopup-image" src={props.image_url}></img>
             </div>
-            </a>
+            </a> */}
+
+            <BrowserRouter>
+                <Link to={`/toiletdetails/${props._id}`}>
+                <div className='InfoPopup-imageContainer'>
+                <img className="InfoPopup-image" src={props.image_url}></img>
+                </div>
+                </Link>
+            </BrowserRouter>
             
             <p><FaMapMarkerAlt style={{color:'#1184e8'}}/> {props.location}</p>
             {/* <p>{props.rating} stars</p> */}
